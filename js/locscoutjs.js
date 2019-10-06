d0775dcf64d820bdcf608ed1e0e21d631680aa40
JeroenDR <jeroenderyck@hotmail.com>
2019-10-06 15:13:46 -0400

    Loc Scout v0.01

---
+++ js/locscoutjs.js
@@ -0,0 +1,49 @@
+/*function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}*/
+/*var map = new ol.Map({
+    target: 'map',
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([-73.99294, 40.7301]),
+      zoom: 14
+    })
+  });
+*/
+debugger;
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVyb2VuZHIiLCJhIjoiY2sxZmN4cjZoMGJtZjNvbnJ4am1zeDUxMCJ9.L3kQnc_CZ6M6YsIEOwyzpg';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  //style: 'mapbox://styles/mapbox/dark-v10',
+  style: 'mapbox://styles/jeroendr/cjzfznutc1osh1cq5qeciw1zr',
+  center: [-73.999, 40.728],
+  zoom: 13
+});
+
+map.on('mousemove', function(e) {
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['movieloc-nyc'] // replace this with the name of the layer
+  });
+
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+    .addTo(map);
+});
