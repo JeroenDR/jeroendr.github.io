08166a184062544ddaf6c307573e2e148cb715bb
JeroenDR <jeroenderyck@hotmail.com>
2019-10-17 21:34:07 -0400

    FilmScout v0.01

--- js/locscoutjs.js
+++ js/locscoutjs.js
@@ -1,26 +1,9 @@
-/*function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}*/
-/*var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([-73.99294, 40.7301]),
-      zoom: 14
-    })
-  });
-*/
 mapboxgl.accessToken = 'pk.eyJ1IjoiamVyb2VuZHIiLCJhIjoiY2sxZmY0cWUxMHQ3ZjNscW84NWwzczJ3cyJ9.gJoTbx5cZAq5HBEqZdVwYw';
+
+
+$(window).on('load',function(){
+        $('#splashModal').modal('show');
+    });
 
 var map = new mapboxgl.Map({
   container: 'map',
@@ -30,7 +13,53 @@ var map = new mapboxgl.Map({
   zoom: 13
 });
 
-map.on('mousemove', function(e) {
+map.addControl(new mapboxgl.GeolocateControl({
+positionOptions: {
+enableHighAccuracy: true
+},
+trackUserLocation: true
+}));
+
+function isWideScreen() {
+   if(window.innerWidth >= 800) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+function openNav() {
+  if(isWideScreen()){
+
+    $("#detailPanel").width("25%");
+    $("#detailPanel").height("100%");
+    $("#map").css("marginLeft = 25%");
+    $("#closebtn").html("<");
+  }else{
+    $("#detailPanel").height("25%");
+    $("#detailPanel").width("100%");
+    $("#detailPanel").css({'bottom' : '0'});
+    $("#map").css({'marginBottom' : "25%"});
+    $("#closebtn").html("v");
+  }
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  //TODO: cleanup with jquery
+  if(isWideScreen()){
+    document.getElementById("detailPanel").style.width = "0";
+    document.getElementById("map").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }else{
+    document.getElementById("detailPanel").style.height = "0";
+    document.getElementById("map").style.marginBottom= "0";
+    document.body.style.backgroundColor = "white";
+  }
+}
+
+map.on('click', function(e) {
+
   var features = map.queryRenderedFeatures(e.point, {
     layers: ['movieloc-nyc'] // replace this with the name of the layer
   });
@@ -40,9 +69,12 @@ map.on('mousemove', function(e) {
   }
 
   var feature = features[0];
-
-  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+openNav();
+$("#movieTitle").html(feature.properties.title);
+$("#movieDescription").html(feature.properties.description);
+//$("headerInfo").val();
+/*  var popup = new mapboxgl.Popup({ offset: [0, -15] })
     .setLngLat(feature.geometry.coordinates)
     .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-    .addTo(map);
+    .addTo(map);*/
 });
