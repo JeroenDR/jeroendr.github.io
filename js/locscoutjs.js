4c6be8e2a336fc9f8da3dccad741a9aa4334fce1
JeroenDR <jeroenderyck@hotmail.com>
2019-10-17 21:47:37 -0400

    new key

--- js/locscoutjs.js
+++ js/locscoutjs.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamVyb2VuZHIiLCJhIjoiY2sxZmY0cWUxMHQ3ZjNscW84NWwzczJ3cyJ9.gJoTbx5cZAq5HBEqZdVwYw';
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVyb2VuZHIiLCJhIjoiY2sxdmgzaW40MGgwYTNjcDUyMG45ODI5cyJ9.tyU7MRdx8PZ_ZGnSbe3MEw';
 
 
 $(window).on('load',function(){
