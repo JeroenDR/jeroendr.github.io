cc6c44c325bd738353a735a317b5bab4cb019e43
jeroendr <jeroenderyck@hotmail.com>
2020-02-17 13:17:35 -0500

    key in seperate file

---
+++ js/license.js
@@ -0,0 +1,1 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVyb2VuZHIiLCJhIjoiY2sxdmgzaW40MGgwYTNjcDUyMG45ODI5cyJ9.tyU7MRdx8PZ_ZGnSbe3MEw';
--- js/locscoutjs.js
+++ js/locscoutjs.js
@@ -1,4 +1,3 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamVyb2VuZHIiLCJhIjoiY2sxdmgzaW40MGgwYTNjcDUyMG45ODI5cyJ9.tyU7MRdx8PZ_ZGnSbe3MEw';
 var isSidePanelOpen = false;
 
 $(window).on('load',function(){
